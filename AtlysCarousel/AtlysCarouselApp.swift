728857261f76442c9a80c941312a98d882a3dcfc
Keshu from Raitech Labs <raitechlabs@gmail.com>
2025-04-11 23:35:43 +0530

    Initial Commit

---
+++ AtlysCarousel/AtlysCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlysCarouselApp.swift
+//  AtlysCarousel
+//
+//  Created by Keshu Rai on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlysCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
